0f40dd39b7da6ef6181990ea4442174c68ebad92
Tomasz Gaładyk <tomasz.galadyk@gmail.com>
2017-10-25 09:27:55 +0200

    uploader for open orders generated and mounted

--- app/models/open_order_file.rb
+++ app/models/open_order_file.rb
@@ -1,3 +1,4 @@
 class OpenOrderFile < ApplicationRecord
+    mount_uploader :file, OpenOrderFileUploader
     has_many :open_orders
 end
--- app/uploaders/open_order_file_uploader.rb
+++ app/uploaders/open_order_file_uploader.rb
@@ -1,4 +1,4 @@
-class OrderFileUploader < CarrierWave::Uploader::Base
+class OpenOrderFileUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
@@ -11,7 +11,6 @@ class OrderFileUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    #"uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     "uploads/#{model.class.to_s.underscore}/"
   end
 
